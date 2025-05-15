968c28cee3380b0886ae4664383116aace093c59
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2025-05-15 12:35:56 +0530

    Delete com/bank directory

--- com/bank/Main.java
+++
@@ -1,8 +0,0 @@
-package com.bank;
-
-public class Main {
-    public static void main(String[] args) {
-        Bank bank = new Bank();
-        bank.start();
-    }
-}
--- com/bank/TransactionService.java
+++
@@ -1,6 +0,0 @@
-package com.bank;
-
-public interface TransactionService {
-    void deposit(double amount);
-    void withdraw(double amount);
-}
